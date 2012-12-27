1d582a57706e1288465d08b4315003df4ee99a2c
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2012-12-26 21:46:41 -0300

    convert to spaces

--- bin/yogi-alloy.js
+++ bin/yogi-alloy.js
@@ -10,146 +10,146 @@
 
 (function() {
 
-	var argv = require('optimist').argv,
-		path = require('path'),
+    var argv = require('optimist').argv,
+        path = require('path'),
 
-		YOGI_PATH = process.env.YOGI_PATH,
-		YOGI_ALLOY_PATH = __dirname + '/../';
+        YOGI_PATH = process.env.YOGI_PATH,
+        YOGI_ALLOY_PATH = __dirname + '/../';
 
-	if (!YOGI_PATH) {
-		console.log('This should be executed from yogi');
-		process.exit(1);
-	}
+    if (!YOGI_PATH) {
+        console.log('This should be executed from yogi');
+        process.exit(1);
+    }
 
-	var requireAlloy = function(p) {
-			return require(path.join(YOGI_ALLOY_PATH, p));
-		},
+    var requireAlloy = function(p) {
+            return require(path.join(YOGI_ALLOY_PATH, p));
+        },
 
-		requireYogi = function(p) {
-			return require(path.join(YOGI_PATH, p));
-		},
+        requireYogi = function(p) {
+            return require(path.join(YOGI_PATH, p));
+        },
 
-		compass = requireAlloy('lib/compass'),
-		file = requireAlloy('lib/file'),
+        compass = requireAlloy('lib/compass'),
+        file = requireAlloy('lib/file'),
 
-		git = requireYogi('lib/git'),
-		log = requireYogi('lib/log'),
-		util = requireYogi('lib/util');
+        git = requireYogi('lib/git'),
+        log = requireYogi('lib/log'),
+        util = requireYogi('lib/util');
 
-	var Alloy = {
+    var Alloy = {
 
-		REGEX_CSS_COMMENTS: /((?:\/\*(?:[^*]|(?:\*+[^*\/]))*\*+\/)|(?:\/\/.*))/g,
-		REGEX_CSS_CLASSES: /([^0-9])\./g,
-		REGEX_CSS_EXTENSION: /\.css$/i,
-		TWITTER_BOOTSTRAP: 'alloy-twitter-bootstrap',
+        REGEX_CSS_COMMENTS: /((?:\/\*(?:[^*]|(?:\*+[^*\/]))*\*+\/)|(?:\/\/.*))/g,
+        REGEX_CSS_CLASSES: /([^0-9])\./g,
+        REGEX_CSS_EXTENSION: /\.css$/i,
+        TWITTER_BOOTSTRAP: 'alloy-twitter-bootstrap',
 
-		/*
-		* YOGI Alloy Commands
-		*/
-		doCompileCss: function(payload, parsed) {
-			var instance = this;
+        /*
+        * YOGI Alloy Commands
+        */
+        doCompileCss: function(payload, parsed) {
+            var instance = this;
 
-			if (instance._isTwitterBootstrapFolder()) {
-				compass.run('compile', [ 'lib/bootstrap.scss', 'lib/responsive.scss' ]);
-				compass.run('compile', [ 'lib/bootstrap.scss', 'lib/responsive.scss' ], { 'output-style': 'compressed' });
-			}
-		},
+            if (instance._isTwitterBootstrapFolder()) {
+                compass.run('compile', [ 'lib/bootstrap.scss', 'lib/responsive.scss' ]);
+                compass.run('compile', [ 'lib/bootstrap.scss', 'lib/responsive.scss' ], { 'output-style': 'compressed' });
+            }
+        },
 
-		doNamespaceCss: function(payload, parsed) {
-			var instance = this,
-				root = git.findRoot() + '/../',
-				files;
+        doNamespaceCss: function(payload, parsed) {
+            var instance = this,
+                root = git.findRoot() + '/../',
+                files;
 
-			if (typeof payload !== "string") {
-				log.bail(util.bad + ' You must specify a namespace.');
-				process.exit(1);
-			}
+            if (typeof payload !== "string") {
+                log.bail(util.bad + ' You must specify a namespace.');
+                process.exit(1);
+            }
 
-			if (instance._isTwitterBootstrapFolder()) {
-				files = file.find(root, instance.REGEX_CSS_EXTENSION);
+            if (instance._isTwitterBootstrapFolder()) {
+                files = file.find(root, instance.REGEX_CSS_EXTENSION);
 
-				files.forEach(function(filename) {
-					var filepath = root + filename,
-						comments = [],
-						commentsIndex = 0;
+                files.forEach(function(filename) {
+                    var filepath = root + filename,
+                        comments = [],
+                        commentsIndex = 0;
 
-					file.replaceRegex(filepath, instance.REGEX_CSS_COMMENTS, function(match) {
-						comments.push(match);
-						return '@' + (commentsIndex++) + '@';
-					});
+                    file.replaceRegex(filepath, instance.REGEX_CSS_COMMENTS, function(match) {
+                        comments.push(match);
+                        return '@' + (commentsIndex++) + '@';
+                    });
 
-					file.replaceRegex(filepath, instance.REGEX_CSS_CLASSES, "$1." + payload + "-");
-					file.replaceTokens(filepath, comments);
-				});
+                    file.replaceRegex(filepath, instance.REGEX_CSS_CLASSES, "$1." + payload + "-");
+                    file.replaceTokens(filepath, comments);
+                });
 
-				log.info(util.good + ' file(s): ' + files.join(', '));
-			}
-		},
+                log.info(util.good + ' file(s): ' + files.join(', '));
+            }
+        },
 
-		doWatchCss: function(payload, parsed) {
-			var instance = this;
+        doWatchCss: function(payload, parsed) {
+            var instance = this;
 
-			if (instance._isTwitterBootstrapFolder()) {
-				compass.run('watch', [ 'lib/bootstrap.scss', 'lib/responsive.scss' ]);
-				compass.run('watch', [ 'lib/bootstrap.scss', 'lib/responsive.scss' ], { 'output-style': 'compressed' });
-			}
-		},
-		/*
-		* End of commands
-		*/
+            if (instance._isTwitterBootstrapFolder()) {
+                compass.run('watch', [ 'lib/bootstrap.scss', 'lib/responsive.scss' ]);
+                compass.run('watch', [ 'lib/bootstrap.scss', 'lib/responsive.scss' ], { 'output-style': 'compressed' });
+            }
+        },
+        /*
+        * End of commands
+        */
 
-		_isReservedArg: function(word) {
-			var reserved = {
-				'$0': 1,
-				'_': 1
-			};
+        _isReservedArg: function(word) {
+            var reserved = {
+                '$0': 1,
+                '_': 1
+            };
 
-			return reserved.hasOwnProperty(word);
-		},
+            return reserved.hasOwnProperty(word);
+        },
 
-		_isTwitterBootstrapFolder: function() {
-			var instance = this,
-				origin = git.origin();
+        _isTwitterBootstrapFolder: function() {
+            var instance = this,
+                origin = git.origin();
 
-			if (origin.indexOf(instance.TWITTER_BOOTSTRAP) > -1) {
-				return true;
-			}
+            if (origin.indexOf(instance.TWITTER_BOOTSTRAP) > -1) {
+                return true;
+            }
 
-			log.bail(util.bad + ' You must be inside ' + instance.TWITTER_BOOTSTRAP + ' repo for this to work!');
+            log.bail(util.bad + ' You must be inside ' + instance.TWITTER_BOOTSTRAP + ' repo for this to work!');
 
-			return false;
-		},
+            return false;
+        },
 
-		_toPascalCase: function(str) {
-			return str.replace(/\w+/g, function(m) {
-				return m[0].toUpperCase() + m.slice(1).toLowerCase();
-			});
-		}
-	};
+        _toPascalCase: function(str) {
+            return str.replace(/\w+/g, function(m) {
+                return m[0].toUpperCase() + m.slice(1).toLowerCase();
+            });
+        }
+    };
 
-	/*
-	* Available commands:
-	*
-	* yogi alloy --compile-css
-	* yogi alloy --watch-css
-	* yogi alloy --namespace-css
-	*/
-	Object.keys(argv).forEach(function(action) {
+    /*
+    * Available commands:
+    *
+    * yogi alloy --compile-css
+    * yogi alloy --watch-css
+    * yogi alloy --namespace-css
+    */
+    Object.keys(argv).forEach(function(action) {
 
-		if (!Alloy._isReservedArg(action)) {
-			var payload = argv[action],
-				methodName = 'do' + Alloy._toPascalCase(action).replace('-', '');
+        if (!Alloy._isReservedArg(action)) {
+            var payload = argv[action],
+                methodName = 'do' + Alloy._toPascalCase(action).replace('-', '');
 
-			if (Alloy[methodName]) {
-				log.info(util.good + ' Running ' + action);
+            if (Alloy[methodName]) {
+                log.info(util.good + ' Running ' + action);
 
-				Alloy[methodName].call(Alloy, payload, argv);
-			}
-			else {
-				log.bail(util.bad + ' Ops, ' + action + ' is not recognized as a valid action');
-			}
-		}
+                Alloy[methodName].call(Alloy, payload, argv);
+            }
+            else {
+                log.bail(util.bad + ' Ops, ' + action + ' is not recognized as a valid action');
+            }
+        }
 
-	});
+    });
 
 }());
