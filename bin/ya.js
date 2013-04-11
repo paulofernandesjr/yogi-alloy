ee34ec7942606323745fa9eae0945254467ed3e0
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2013-04-11 15:12:52 -0700

    Adding 'alloy' to yogi arguments

--- bin/ya.js
+++ bin/ya.js
@@ -7,6 +7,9 @@ var command = require('command'),
 // removing 'node' and the name of the JavaScript file.
 argv.splice(0, 2);
 
+// adding 'alloy' to yogi arguments
+argv.unshift('alloy');
+
 command.open()
     .on('stdout', command.writeTo(process.stdout))
     .on('stderr', command.writeTo(process.stderr))
