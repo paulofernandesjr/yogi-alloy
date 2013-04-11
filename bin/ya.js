d625e1fdf460afedfe418c13a1dcedfd96396c7a
Zeno Rocha <zno.rocha@gmail.com>
2013-04-11 15:46:27 -0700

    Fix ya prompt answers

--- bin/ya.js
+++ bin/ya.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 
-var command = require('command'),
+var spawn = require('child_process').spawn,
     which = require('which').sync,
     argv = process.argv;
 
 // removing 'node' and the name of the JavaScript file.
 argv.splice(0, 2);
-
 // adding 'alloy' to yogi arguments
 argv.unshift('alloy');
 
-command.open()
-    .on('stdout', command.writeTo(process.stdout))
-    .on('stderr', command.writeTo(process.stderr))
-    .exec(which('yogi'), argv);
+spawn(which('yogi'), argv, {
+  cwd: process.cwd(),
+  env: process.env,
+  stdio: 'inherit'
+});
